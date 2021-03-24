3c4f01dd2ed3087bb9bbdf12633e66d006c89986
vincentthh35 <vincentthh35@gmail.com>
2021-03-24 12:28:55 +0800

    [hw3] sample code

---
+++ hw3/src/containers/TodoApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from "../components/Header";
+
+class TodoApp extends Component {
+    render() {
+        return (
+            <>
+                <Header text="todos" />
+            </>
+        );
+    }
+}
+
+export default TodoApp;
