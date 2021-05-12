70969a8862fff6b55fe28127de0b2966b5773db8
vincentthh35 <vincentthh35@gmail.com>
2021-05-12 14:04:06 +0800

    [hw6] init commit

---
+++ hw6/own/backend/server.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+import dotenv from "dotenv-defaults";
+dotenv.config()
+
+mongoose
+    .connect(
+        process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then((res) => console.log('mongodb connection created'));
